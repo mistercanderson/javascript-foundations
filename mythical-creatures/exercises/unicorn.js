e04715fe9f3b545a23485dad2f9a071885dfc7d9
Chris Anderson <mistercanderson@gmail.com>
2021-02-08 17:16:38 -0700

    Solve unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,18 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
-  }
-}
+  };
+  isWhite() {
+    if (this.color === 'white') {
+      return true
+    } else {
+      return false
+    };
+  };
+  says(phrase) {
+    return `**;* ${phrase} *;**`
+  };
+};
 
 module.exports = Unicorn;
