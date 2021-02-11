a749b85f15a8635f339c2ea00ba273c6379eb13d
Chris Anderson <mistercanderson@gmail.com>
2021-02-10 21:31:08 -0700

    Solve Dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,18 @@
-
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.mealCount = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+    this.mealCount++;
+    if (this.mealCount > 2) {
+      this.hungry = false;
+    }
+  }
+}
+module.exports = Dragon
