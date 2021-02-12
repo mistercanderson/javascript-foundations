73050ac5a30b3cc1a21503600782d147ec586ff6
Chris Anderson <mistercanderson@gmail.com>
2021-02-11 19:13:47 -0700

    Solve Medusa tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,21 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  };
+  gazeAtVictim(victim) {
+    if (this.statues.length > 2) {
+      this.statues.push(new Statue(victim.name));
+      var person = new Person(this.statues[0].name);
+      person.mood = 'relieved'
+      this.statues.shift();
+      return person
+    };
+    this.statues.push(new Statue(victim.name));
+  };
+
+};
+module.exports = Medusa
