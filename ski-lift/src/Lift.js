f57c17e8a273a717d55b09e9f8a426c64ffca1d7
Chris Anderson <mistercanderson@gmail.com>
2021-02-16 14:54:13 -0700

    Work on ski-lift tests

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,22 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+  admitSkier(name, hasLiftTicket) {
+    var skier = new Skier(name, hasLiftTicket);
+    if (this.skiers.length < this.limit && skier.hasLiftTicket) {
+      this.skiers.push(skier);
+    } else if (!skier.hasLiftTicket) {
+      return `Sorry, ${skier.name}. You need a lift ticket!`
+    } else {
+      return `Sorry, ${skier.name}. Please wait for the next lift!`;
+    }
+  }
 }
 
 module.exports = Lift;
--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,21 @@
 class Skier {
-
+  constructor(name, hasLiftTicket) {
+    this.name = name;
+    this.hasLiftTicket = hasLiftTicket;
+    this.skillLevel = 1;
+    this.nextSlope = 'green circle';
+  }
+  takeLesson() {
+    this.skillLevel++
+  }
+  pickSlope() {
+    if (this.skillLevel > 2) {
+      this.nextSlope = 'blue square';
+    }
+    if (this.skillLevel > 4) {
+      this.nextSlope = 'black diamond';
+    }
+  }
 }
 
 module.exports = Skier;
