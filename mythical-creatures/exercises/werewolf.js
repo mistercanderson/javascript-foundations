c68ae46ec8262d7b383bf96ef10bb2322160bc82
Chris Anderson <mistercanderson@gmail.com>
2021-02-26 11:34:26 -0700

    Solve Werewolf test

---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,29 @@
+class Werewolf {
+  constructor(name) {
+    this.name = name;
+    this.form = 'human';
+    this.hungry = false;
+  }
+  completeTransformation() {
+    if (this.form === 'human') {
+      this.form = 'wolf';
+      this.hungry = true;
+      return `Aaa-Woooo!`
+    } else {
+      this.form = 'human';
+      this.hungry = false;
+      return `Where are I?`
+    }
+  }
+  eatVictim(victim) {
+    if (this.form === 'wolf') {
+      victim.alive = false;
+      this.completeTransformation();
+      return `Yum, ${victim.name} was delicious.`
+    }
+    return `No way am I eating Baby, I'd like a burger!`;
+  }
+
+
+}
+module.exports = Werewolf
