af01fd027ac4e8d61508b400c110bb4e98e8eb8e
Chris Anderson <mistercanderson@gmail.com>
2021-03-02 12:51:07 -0700

    Solve Craft/Material test

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,21 @@
 class Material {
-
+  constructor(name, price, amount, units) {
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+    this.units = units
+  }
+  useMaterial(amt) {
+    if (this.amount > amt) {
+      this.amount -= amt;
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+    } else {
+      return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+    }
+  }
+  calculateMaterialCost() {
+    return this.amount * this.price
+  }
 }
 
 module.exports = Material;
