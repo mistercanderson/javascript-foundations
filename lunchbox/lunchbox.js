6f8460fd996642dec33ca7111ab97d9c616ce22c
Chris Anderson <mistercanderson@gmail.com>
2021-03-02 14:28:19 -0700

    Solve Snack/Lunchbox test

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,24 @@
 class LunchBox {
-
+  constructor(obj) {
+    this.owner = obj.owner;
+    this.material = obj.madeOf;
+    this.shape = obj.shape;
+    this.color = obj.color;
+    this.snacks = []
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks
+  }
 }
 
 module.exports = LunchBox;
